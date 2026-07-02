cbe12d534b9a4fe04219d81eeccbecb13d612374
Daksh Dagar <dakshdagar1628@users.noreply.github.com>
2026-07-02 17:05:53 +0530

    chore: execute whole-repo ponytail audit, stripping 35 dead packages and 17 unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ScrollToTop from "@/components/ScrollToTop";
 import AlgoVizHome from "./pages/AlgoVizHome";
@@ -13,31 +12,27 @@ import LinkedListModule from "./pages/LinkedListModule";
 import DoublyLinkedListModule from "./pages/DoublyLinkedListModule";
 import NotFound from "./pages/NotFound";
 
-const queryClient = new QueryClient();
-
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <ScrollToTop />
-          <Routes>
-            <Route path="/" element={<AlgoVizHome />} />
-            <Route path="/algoviz" element={<AlgoVizHome />} />
-            <Route path="/algoviz/arrays" element={<ArraysModule />} />
-            <Route path="/algoviz/strings" element={<StringsModule />} />
-            <Route path="/algoviz/stack" element={<StackModule />} />
-            <Route path="/algoviz/queue" element={<QueueModule />} />
-            <Route path="/algoviz/linked-list" element={<LinkedListModule />} />
-            <Route path="/algoviz/doubly-linked-list" element={<DoublyLinkedListModule />} />
-            {/* All custom routes must be defined above the catch-all route */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </TooltipProvider>
-    </QueryClientProvider>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<AlgoVizHome />} />
+          <Route path="/algoviz" element={<AlgoVizHome />} />
+          <Route path="/algoviz/arrays" element={<ArraysModule />} />
+          <Route path="/algoviz/strings" element={<StringsModule />} />
+          <Route path="/algoviz/stack" element={<StackModule />} />
+          <Route path="/algoviz/queue" element={<QueueModule />} />
+          <Route path="/algoviz/linked-list" element={<LinkedListModule />} />
+          <Route path="/algoviz/doubly-linked-list" element={<DoublyLinkedListModule />} />
+          {/* All custom routes must be defined above the catch-all route */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
   );
 }
 
